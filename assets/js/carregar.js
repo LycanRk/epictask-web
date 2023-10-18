8cdfb1ce235c9d78f07c2b7cf86d9f70c9e2bde1
Pedro Henrique Vieira <phvieira83@gmail.com>
2023-10-18 19:15:07 -0300

    att1

---
+++ assets/js/carregar.js
@@ -0,0 +1,33 @@
+window.addEventListener("load", () => {
+    let tarefas = JSON.parse(localStorage.getItem("tarefas")) || []
+
+    
+    tarefas.forEach(tarefa => criarCard(tarefa))
+    
+    
+})
+
+function criarCard(tarefa) {
+    const card = document.createElement("div")
+    
+    card.innerHTML = `
+    <div class="card">
+    <div class="card-content">
+    <span class="card-title">${tarefa.titulo}</span>
+    <p> ${tarefa.descricao}</p>
+    <span data-badge-caption="pontos" class="badge blue white-text">${tarefa.pontos}</span>
+    </div>
+    <div class="card-action">   
+    <a href="#" class="btn red">
+    <i class="material-icons">delete_forever</i>
+    </a>
+    <a href="#" class="btn green">
+    <i class="material-icons">check</i>
+    </a>
+    </div>
+    </div>
+    `
+    console.log(card)
+
+    document.querySelector("#lista-tarefas").appendChild(card)
+}
